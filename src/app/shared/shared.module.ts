3a2ccd4938415b100924f70946f374275f87e77e
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-10-25 21:40:17 -0500

    adding assets

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
